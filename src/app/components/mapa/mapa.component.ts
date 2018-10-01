ec6578450f8a10cf9e29fa34605d82238e185336
Marvin Ajcuc <marvinajcuc@gmail.com>
2018-09-30 22:49:50 -0600

    correcciones

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -3,7 +3,7 @@ import { Marcador } from '../../classes/marcador.class';
 import {MatSnackBar} from '@angular/material';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { MapaEditarComponent } from './mapa-editar.component';
-import {} from '@types/googlemaps';
+ import {} from '@types/googlemaps';
 
 @Component({
   selector: 'app-mapa',
@@ -179,9 +179,15 @@ export class MapaComponent implements OnInit {
     let point1 = from;
     let point2 = to;
     let lenght=google.maps.geometry.spherical.computeDistanceBetween(point1,point2);
+    if(interval>=lenght){
+      console.log('int>=lenght:',interval,'>=',lenght);
+      return;
+    }
     let interpolateFactor = interval/lenght;
+    console.log('interpolatefactor: ',interpolateFactor);
     for (let i=0; i < lenght; i++) {
       let point = google.maps.geometry.spherical.interpolate(point1, point2, interpolateFactor*i);
+      console.log('interpolateFactor*i: ',interpolateFactor*i);
       this.tempPuntos.push(point);
       // this.marcadores.push(new Marcador(point.lat(),point.lng()));
     }
@@ -198,19 +204,36 @@ export class MapaComponent implements OnInit {
     for(let i=0;i<this.puntos.length-1;i++){
       this.createTemporalPoints(new google.maps.LatLng(this.puntos[i].lat, this.puntos[i].lng),new google.maps.LatLng(this.puntos[i+1].lat, this.puntos[i+1].lng),1);
     }
+    this.createTemporalPoints(new google.maps.LatLng(this.puntos[this.puntos.length-1].lat, this.puntos[this.puntos.length-1].lng),new google.maps.LatLng(this.puntos[0].lat, this.puntos[0].lng),1);
+    console.log('tempuntos.lenght:',this.tempPuntos.length);
+    console.log('tempuntos:',this.tempPuntos);
     let minDistance=9999999999999;
+    let subMinDist = minDistance;
     let minDistPoint:google.maps.LatLng;
     for(let i=0;i<this.tempPuntos.length;i++) {
       let dist=google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(this.marcadores[0].lat, this.marcadores[0].lng), this.tempPuntos[i]);
       if(dist<minDistance){
         minDistPoint=this.tempPuntos[i];
+        subMinDist = minDistance;
         minDistance=dist;
       }  
+      console.log('dist ', i , ': ' ,dist);
+    }
+    if(this.tempPuntos.length<this.paths.length){
+      for(let i=0;i<this.paths.length;i++) {
+        let temPoint=new google.maps.LatLng(this.paths[i].lat,this.paths[i].lng);
+        let dist=google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(this.marcadores[0].lat, this.marcadores[0].lng),temPoint );
+        if(dist<minDistance){
+          minDistPoint=temPoint;
+          subMinDist = minDistance;
+          minDistance=dist;
+        }  
+      }
     }
     this.minDist=[];
     this.minDist.push(new google.maps.LatLng(this.marcadores[0].lat, this.marcadores[0].lng));
     this.minDist.push(minDistPoint);
-
+    console.log('mindistance: ',minDistance,', submindist: ',subMinDist);
     // this.polygon = new google.maps.Polygon({paths: this.paths});
     //   // this.polygon.setPaths(this.paths);
     // const containsLocation = google.maps.geometry.poly.containsLocation(
